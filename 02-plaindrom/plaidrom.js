7f1a4dbb36c0e9264289b614d679e7115c1d0688
arefeh montazeri <montazeriarefeh70s@gmail.com>
2024-09-04 19:56:23 +0330

    EX02 is Done

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,19 @@
 // write your func here
+
+
+function reverseString(str) {
+    let splitString = str.split(""); 
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+ return joinArray;
+}
+
+function plaindrom(str) {
+    let reverceString = str.split("").reverse().join(""); 
+    if(str===reverceString){
+        return true
+    } else {
+        return false
+    }
+}
+console.log( plaindrom("Test"))
