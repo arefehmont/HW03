441f18466b2b10093cf84e33db9d41a38ce52e9a
arefeh montazeri <montazeriarefeh70s@gmail.com>
2024-09-04 20:27:41 +0330

    EX05 is Done

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,12 @@
 // write your func here
+
+
+const TestArr = [3, 1 , 12369.5 , 4, 1, 5, 9, 2, 6, 5, 3 , 412];
+
+let max = TestArr[0];
+for (let i = 1; i < TestArr.length; i++) {
+  if (TestArr[i] > max) {
+    max = TestArr[i];
+  }
+}
+console.log(max); 
