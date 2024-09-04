2b8d2316ac8e3884bce87805bb464d0e5a5dd1ce
arefeh montazeri <montazeriarefeh70s@gmail.com>
2024-09-04 19:48:14 +0330

    EX01 is Done

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,21 @@
-// write your func here
+
+// use methods
+
+function reverseString(str) {
+    let splitString = str.split(""); 
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join("");
+ return joinArray;
+}
+
+// use for Loop
+
+function reverseString(str) {
+    let RevString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        RevString += str[i];
+    }
+    return RevString;
+}
+const test = reverseString('Test For reverseString')
+console.log(test);
