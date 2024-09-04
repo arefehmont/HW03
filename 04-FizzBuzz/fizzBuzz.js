a9b7bd59c4045b51d2e9b7bddf9871cb32d1a1ad
arefeh montazeri <montazeriarefeh70s@gmail.com>
2024-09-04 20:14:53 +0330

    EX04 is Done

--- 04-FizzBuzz/fizzBuzz.js
+++ 04-FizzBuzz/fizzBuzz.js
@@ -1,1 +1,9 @@
 // write your func here
+
+function FizzBuzz() { 
+    for (let index = 1; index <101; index++) {
+    if (index%3 == 0) {console.log("fizz")}
+    else if (index%5 == 0){console.log("buzz")}
+    else if (index%5==0 && index%3 == 0 ){console.log("fizzbuzz")} } }
+
+    console.log(FizzBuzz(9))
