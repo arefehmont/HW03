0de9c37a1c04000dcb1c7e1777615a6ca3964737
arefeh montazeri <montazeriarefeh70s@gmail.com>
2024-09-04 20:06:30 +0330

    EX03  is Done

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,13 @@
 // write your func here
+
+
+function factorialize(num) {
+    if (num < 0) 
+          return -1;
+    else if (num == 0 || n == 1) 
+        return 1;
+    else {
+        return (num * factorialize(num - 1));
+    }
+  }
+  factorialize(5);
